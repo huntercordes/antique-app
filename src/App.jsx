bdb2d27b2718b2d7f5c98525675531212549fa1d
huntercordes <huntercordes@gmail.com>
2025-03-31 12:26:40 +0200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import WeaponSelection from "./pages/WeaponSelection"; // Placeholder for next step
+import WeaponSelection from "./pages/WeaponSelectionPage"; // Placeholder for next step
 
 function App() {
   return (
